f59c929a7f39470a68bf264b5f70a2d442e70bad
TheNulls <thenellb@oregonstate.edu>
2018-04-09 06:23:20 -0700

    tried implementing a way to search through more than one entry, but it's a bit clunky. Cleaned up

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-// const guild = Discord.Guild('111277854020407296');
 var config = require('./config')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-//   console.log(client);
-//   console.log(guild);
+
 });
+
 client.on('message', msg => {
     if (msg.content !== 'no u'){
         msg.channel.send('no u');
@@ -18,23 +17,17 @@ client.on('message', msg => {
 
 client.on('messageDelete', msg => {
 
-
-    msg.guild.fetchAuditLogs({limit: 1}).then(logs => {
-		var logArray = Array.from(logs.entries.values());
-        var entry = logArray[0];
-
-
-        if (entry.action === 'MESSAGE_DELETE' && entry.executor.username !== entry.target.username){
+    msg.guild.fetchAuditLogs({limit: 10}).then(logs => {
+        var logArray = Array.from(logs.entries.values());       
+        var entry = logArray[0]
+         if (entry.action === 'MESSAGE_DELETE' && entry.executor.username !== entry.target.username){
             let botembed = new Discord.RichEmbed()
                 .setDescription('Message Deleted')
                 .setColor('#15f153')
-                .addField('Sender', entry.target.username)
+                .addField('Sender', `${entry.target.username}#${entry.target.discriminator} (id:  ${entry.target.id})`)
                 .addField('Content', msg.cleanContent);
                 return client.channels.get(config.log).send(botembed);
-            console.log('Message Deleted')
         }
-
-        
 	}).catch(console.error);
 });
 
