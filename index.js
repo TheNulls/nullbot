199cc81299ab3ea658ec1d529b713a9df26c6beb
TheNulls <thenellb@oregonstate.edu>
2018-04-09 06:45:26 -0700

    Added outline of role cleanup and raid protection

--- index.js
+++ index.js
@@ -6,18 +6,24 @@ var config = require('./config')
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-
 });
 
 client.on('message', msg => {
     if (msg.content !== 'no u'){
-        msg.channel.send('no u');
+        // msg.channel.send('no u');
+    }
+    if (msg.content === '!18++cleanup' && msg.channel.id === config.log){
+        console.log('Cleaning up roles!');
+        // clean roles
     }
 });
 
-client.on('messageDelete', msg => {
+client.on('guildMemberAdd', member => {
+    // promise here to count users joining in x time
+});
 
-    msg.guild.fetchAuditLogs({limit: 10}).then(logs => {
+client.on('messageDelete', msg => {
+    msg.guild.fetchAuditLogs({limit: 1}).then(logs => {
         var logArray = Array.from(logs.entries.values());       
         var entry = logArray[0]
          if (entry.action === 'MESSAGE_DELETE' && entry.executor.username !== entry.target.username){
